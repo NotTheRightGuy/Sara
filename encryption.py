2f1fe4a2944d1c82b54ca5d7a7f3e1949b848148
Delsion <janmejaychatterjee@gmail.com>
2020-11-02 09:39:04 +0530

    Minor Changes

--- encryption.py
+++ encryption.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 
+cwd = os.getcwd()
 
 def encrypt():
     os.chdir(cwd)
@@ -12,7 +13,7 @@ def encrypt():
         for textFile in text_files:
             with open(textFile) as file:
                 content = file.read()
-            with open(textFile + ".cd", 'rb') as file:
+            with open(textFile[:-3]+"cd" , 'wb') as file:
                 pickle.dump(content, file)
             os.remove(textFile)
 
@@ -27,5 +28,6 @@ def decrypt():
         for textFile in text_files:
             with open(textFile, 'rb') as file:
                 content = pickle.load(file)
-            with open(textFile + ".txt") as file:
+            with open(textFile[:-2] +"txt",'w') as file:
                 file.write(content)
+            os.remove(textFile)
