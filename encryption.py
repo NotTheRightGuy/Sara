2adffd727f776cca77d86dbd7322087fb4d620a0
Delsion <janmejaychatterjee@gmail.com>
2020-11-02 09:09:34 +0530

    Changed the algo

--- encryption.py
+++ encryption.py
@@ -1,26 +1,5 @@
 import os
-import random
 import pickle
-
-L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
-I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-
-cwd = str(os.getcwd())
-
-def caeser(text,key):
-    ciphertext = ""
-    for c in text.upper():
-        if c.isalpha(): ciphertext += I2L[(L2I[c] + key)%26 ]
-        else: ciphertext += c
-
-    return ciphertext
-
-def uncaeser(text,key):
-    plaintext = ""
-    for c in text.upper():
-        if c.isalpha(): plaintext += I2L[(L2I[c] - key) %26]
-        else: plaintext += c
-    return plaintext
 
 
 def encrypt():
@@ -33,15 +12,10 @@ def encrypt():
         for textFile in text_files:
             with open(textFile) as file:
                 content = file.read()
-            key = random.randint(11,99)
-            content_to_write = caeser(content,key)
-            with open(textFile,'w') as file:
-                file.write(content_to_write)
-                file.write("\nKEY:{}".format(key))
-            with open(textFile) as file:
-                content_to_be_rewritten = file.read()
-            with open(textFile,'wb') as file:
-                pickle.dump(content_to_be_rewritten,file)
+            with open(textFile + ".cd", 'rb') as file:
+                pickle.dump(content, file)
+            os.remove(textFile)
+
 
 def decrypt():
     os.chdir(cwd)
@@ -51,14 +25,7 @@ def decrypt():
         os.chdir(folder)
         text_files = os.listdir()
         for textFile in text_files:
-            with open(textFile,'rb') as file:
+            with open(textFile, 'rb') as file:
                 content = pickle.load(file)
-            key = int(content[-2:])
-            content_to_rewrite = uncaeser(content,key)
-            content_to_rewrite = content_to_rewrite[:-6]
-            with open(textFile,'w') as file:
-                file.write(content_to_rewrite)
-
-
-
-
+            with open(textFile + ".txt") as file:
+                file.write(content)
